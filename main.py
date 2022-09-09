1e3a254df98075e6dcce71e2d48384bb51bbf59c
Arseny <92612253+z4pik@users.noreply.github.com>
2022-09-09 12:05:13 +0300

    v 5.0
    fix_prev_mistake

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def download_images(imagelinks):
         file.write(response.content)
 
 
-# Получаем адрес страницы 
+# Получаем адрес страницы
 page = requests.get(f'https://the-moment.ru/sitemap.html')
 soup = BeautifulSoup(page.text, 'lxml')
 
@@ -30,26 +30,27 @@ for posts_in_month in soup.select('td > a')[1::]:
 
     #вытаскиваем пост из каждого месяца
     for post in soup.select('td > a'):
+        post_page = requests.get(post['href'])
+        post_soup = BeautifulSoup(post_page.text, 'lxml')
         print(f"{post['href']}\n\n")
+        # print(post_soup)
 
-
-
-        #всё что находится ниже надо доработать 
-        for elem in (single_post_soup.select(".site-content > .site-content-inner > .content-area > .site-main > article > .post-cards post-cards--grid")):
+        for elem in (post_soup.select(".site-content > .site-content-inner > .content-area > .site-main > article")):
             print('Получаем заголовок статьи')
             title = elem.select(".entry-header > h1")
-            print(title[0].text)
-            # получаем текст статьи
+            print(f'{title[0].text}\n\n')
+
             print('Получаем текст статьи')
             text = elem.select(".entry-content")
-            print(text[0].text)
-            # Получаем картинки
+            print(f"{text[0].text}\n\n")
+
             print("Получаем картинки статьи")
 
             imagelinks = []
-            for img in single_post_soup.find_all('img', src=True, ):
-                print(img['src'])
+            for img in elem.find_all('img', src=True, ):
+                print(f"{img['src']}\n\n")
 
                 imagelinks.append(img['src'])
                 download_images(imagelinks)
-        print(f'все данные взяты с данной страниы {single_post_url}')
+
+        print(f"все данные взяты с данной страниы {post['href']}\n\n")
