916ce260ab3680dbfd148da4c0e26c658c97c6c6
Arata <shahzod20030905@gmail.com>
2022-09-10 01:07:54 +0300

    нужно доделать 4 пункт в тз

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 from bs4 import BeautifulSoup
-import requests, os, random, csv, shutil
+import requests
+import os 
+import random
+import csv
+import shutil
 
 
-def download_images(imagelinks):
-    # проверка на наличие папки images
+
+DIR = 'images'
+
+
+def download_images(imagelink):
+
     if not os.path.exists("images"):
         os.mkdir("images")
 
-    for i, imagelink in enumerate(imagelinks):
+
+    try:
         response = requests.get(imagelink)
-
-    imagename = 'images/' + str(i+1) + '.jpg'
-    with open(imagename, 'wb') as file:
-        file.write(response.content)
-
-    # Записываем данные в csv
-    # открываем csv-файл
-    with open('out.csv', mode='a', encoding='utf-8') as w_file:
-        file_writer = csv.writer(w_file, delimiter=";", lineterminator="\r")
-        file_writer.writerow([f"{title[0].text}", f"{text[0].text}", f"{imagename}"])
-
+    except OSError:
+        path = r"random_img"
+        filename = random.choice([
+            x for x in os.listdir(path)
+            if os.path.isfile(os.path.join(path, x))
+        ])
+        shutil.copy(f"random_img/{filename}", f"images/{filename}")
+    else:
+        imagename = f'images/{imagelink.split("/")[-1]}'
+        with open(imagename, 'wb') as file:
+            file.write(response.content)
 
 # Получаем адрес страницы
 page = requests.get(f'https://the-moment.ru/sitemap.html')
@@ -38,42 +47,22 @@ for posts_in_month in soup.select('td > a')[1::]:
         post_page = requests.get(post['href'])
         post_soup = BeautifulSoup(post_page.text, 'lxml')
         print(f"{post['href']}\n\n")
-        # print(post_soup)
+
 
         for elem in (post_soup.select(".site-content > .site-content-inner > .content-area > .site-main > article")):
             print('Получаем заголовок статьи')
             title = elem.select(".entry-header > h1")
-            print(f'{title[0].text}\n\n')
+            # print(f'{title[0].text}\n\n')
 
-            print('Получаем текст статьи')
+            # print('Получаем текст статьи')
             text = elem.select(".entry-content")
-            print(f"{text[0].text}\n\n")
+            # print(f"{text[0].text}\n\n")
 
-            print("Получаем картинки статьи")
+            print("Получаем картинки статьи")   
 
             imagelinks = []
             for img in elem.find_all('img', src=True, ):
                 print(f"{img['src']}\n\n")
-                # Если нет картинки
-                if len(img['src']) < 1:
-                    # Открываем папку с готовым картинками
-                    # Открываем папку с картинками
-                    path = r"random_img"
-                    # Папка где хранятся картинки к статьям
-                    DIR = 'images'
-                    # Выбраем случайную картинку
-                    filename = random.choice([
-                        x for x in os.listdir(path)
-                        if os.path.isfile(os.path.join(path, x))
-                    ])
-                    # Перемещаем её в папку с основными картинками
-                    shutil.copy(f"random_img/{filename}", f"images/{filename}")
-                    # Открываем и записываем данные в csv
-                    with open('out.csv', mode='a', encoding='utf-8') as w_file:
-                        file_writer = csv.writer(w_file, delimiter=";", lineterminator="\r")
-                        file_writer.writerow([f"{title[0].text}", f"{text[0].text}", f"{filename}"])
-                else:
-                    imagelinks.append(img['src'])
-                    download_images(imagelinks)
+                download_images(img['src'])
 
         print(f"все данные взяты с данной страниы {post['href']}\n\n")
