3af6e28150c2fcb00b307190607d5214737d069f
Arseny <92612253+z4pik@users.noreply.github.com>
2022-09-09 15:45:42 +0300

    v 6.1
    add random choice img and csv

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import os
-
+import random
+import csv
 
 
 def download_images(imagelinks):
@@ -16,12 +17,19 @@ def download_images(imagelinks):
     with open(imagename, 'wb') as file:
         file.write(response.content)
 
+    # Записываем данные в csv
+    # открываем csv-файл
+    with open('out.csv', mode='a', encoding='utf-8') as w_file:
+        file_writer = csv.writer(w_file, delimiter=";", lineterminator="\r")
+        file_writer.writerow([f"{title[0].text}", f"{text[0].text}", f"{imagename}"])
+
 
 # Получаем адрес страницы
 page = requests.get(f'https://the-moment.ru/sitemap.html')
 soup = BeautifulSoup(page.text, 'lxml')
 
 print("Получаем ссылки")
+
 
 #заходим в каждый "отдел" где посты рассортированы по месяцам
 for posts_in_month in soup.select('td > a')[1::]:
@@ -49,8 +57,26 @@ for posts_in_month in soup.select('td > a')[1::]:
             imagelinks = []
             for img in elem.find_all('img', src=True, ):
                 print(f"{img['src']}\n\n")
-
-                imagelinks.append(img['src'])
-                download_images(imagelinks)
+                # Если нет картинки
+                if len(img['src']) < 1:
+                    # Открываем папку с готовым картинками
+                    # Открываем папку с картинками
+                    path = r"random_img"
+                    # Папка где хранятся картинки к статьям
+                    DIR = 'images'
+                    # Выбраем случайную картинку
+                    filename = random.choice([
+                        x for x in os.listdir(path)
+                        if os.path.isfile(os.path.join(path, x))
+                    ])
+                    # Перемещаем её в папку с основными картинками
+                    os.replace(f"random_img/{filename}", f"images/{filename}")
+                    # Открываем и записываем данные в csv
+                    with open('out.csv', mode='a', encoding='utf-8') as w_file:
+                        file_writer = csv.writer(w_file, delimiter=";", lineterminator="\r")
+                        file_writer.writerow([f"{title[0].text}", f"{text[0].text}", f"{filename}"])
+                else:
+                    imagelinks.append(img['src'])
+                    download_images(imagelinks)
 
         print(f"все данные взяты с данной страниы {post['href']}\n\n")
